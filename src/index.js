0e37fbe38710f32f07271d3bae729fc0aaa10f8c
Bernardo Raposo <braposo@gmail.com>
2018-02-22 17:06:07 +0000

    Makes changes so we can have custom hues and does lums variation based on base colour

--- src/index.js
+++ src/index.js
@@ -1,105 +1,119 @@
+const chroma = require("chroma-js");
+const hueName = require("./hue-name");
 
-const chroma = require('chroma-js')
-const hueName = require('./hue-name')
+const generateLums = lum => {
+    const l = lum * 10;
+    const integer = Math.floor(l);
+    const decimal = l - integer;
 
-const lums = [
-  9,
-  8,
-  7,
-  6,
-  5,
-  4,
-  3,
-  2,
-  1,
-  0
-]
-  .map(n => n + .5)
-  .map(n => n / 10)
+    // Make sure we always have the base colour as one
+    // of the shades
+    const lums = [0.3 - decimal, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        .map(n => n + decimal)
+        .map(n => n / 10)
+        .sort((a, b) => b - a);
+
+    const selectedLums = [0, 3, 6, 8, 9].map(n => lums[n]);
+
+    return [...selectedLums, ...lums];
+};
 
 const createArray = length => {
-  const arr = []
-  for (let i = 0; i < length; i++) {
-    arr.push(i)
-  }
-  return arr
-}
+    const arr = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(i);
+    }
+    return arr;
+};
 
 const createHues = length => {
-  const hueStep = 360 / length
-  return base => {
-    const hues = createArray(length)
-      .map(n => Math.floor((base + (n * hueStep)) % 360))
+    const hueStep = 360 / length;
+    return base => {
+        const hues = createArray(length).map(n => (base + n * hueStep) % 360);
 
-    return hues
-  }
-}
+        return hues;
+    };
+};
 
 const desat = n => hex => {
-  const [ h, s, l ] = chroma(hex).hsl()
-  return chroma.hsl(h, n, l).hex()
-}
+    const [h, s, l] = chroma(hex).hsl();
+    return chroma.hsl(h, n, l).hex();
+};
 
 const createBlack = hex => {
-  const d = desat(1/8)(hex)
-  return chroma(d).luminance(.05).hex()
-}
+    const d = desat(1 / 8)(hex);
+    return chroma(d)
+        .luminance(0.05)
+        .hex();
+};
 
-const createShades = hex => {
-  return lums.map(lum => {
-    return chroma(hex).luminance(lum).hex()
-  })
-}
+const createShades = (hex, lum) => {
+    // Generates luminance values based on base colour
+    const lms = generateLums(lum);
+
+    // Generates the different shades for each hex
+    return lms.map(lum => {
+        return chroma(hex)
+            .luminance(lum)
+            .hex();
+    });
+};
 
 // Mappers
 const keyword = hex => {
-  const [ hue, sat ] = chroma(hex).hsl()
-  if (sat < .5) {
-    return 'gray'
-  }
-  const name = hueName(hue)
-  return name
-}
+    const [hue, sat] = chroma(hex).hsl();
+    if (sat < 0.5) {
+        return "gray";
+    }
+    const name = hueName(hue);
+    return name;
+};
 
 // Reducer
 const toObj = (a, color) => {
-  const key = a[color.key] ? color.key + '2' : color.key
-  a[key] = color.value
-  return a
-}
+    const key = a[color.key] ? color.key + "2" : color.key;
+    a[key] = color.value;
+    return a;
+};
 
-const palx = (hex, options = {}) => {
-  const color = chroma(hex)
-  const colors = []
-  const [ hue, sat, lte ] = color.hsl()
+const palx = (hex, hues = 12, options = {}) => {
+    const color = chroma(hex);
+    const colors = [];
+    const [hue, sat, lte] = color.hsl();
+    const lum = color.luminance();
 
-  const hues = createHues(12)(hue)
+    // Set number of different colours
+    //const generatedHues = createHues(hues)(hue);
+    const generatedHues = [hue, 120, 48, 30, 268]
 
-  colors.push({
-    key: 'black',
-    value: createBlack('' + color.hex())
-  })
+    generatedHues.forEach(h => {
+        // Get colour with same saturation and lightness than base for each hue
+        const c = chroma.hsl(h, sat, lte);
+        const key = keyword(c);
+        colors.push({
+            key,
+            value: createShades("" + c.hex(), lum),
+        });
+    });
 
-  colors.push({
-    key: 'gray',
-    value: createShades(desat(1/8)('' + color.hex()))
-  })
-
-  hues.forEach(h => {
-    const c = chroma.hsl(h, sat, lte)
-    const key = keyword(c)
     colors.push({
-      key,
-      value: createShades('' + c.hex())
-    })
-  })
+        key: "black",
+        value: createBlack("" + color.hex()),
+    });
 
-  const obj = Object.assign({
-    base: hex,
-  }, colors.reduce(toObj, {}))
+    colors.push({
+        key: "gray",
+        value: createShades(desat(1 / 8)("" + color.hex()), lum),
+    });
 
-  return obj
-}
+    const obj = Object.assign(
+        {
+            base: hex,
+        },
+        colors.reduce(toObj, {})
+    );
 
-module.exports = palx
+    return obj;
+};
 
+module.exports = palx;
