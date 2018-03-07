6422eb37e9b0c8b78f5e2de1f69ad4e4ccbeecba
Bernardo Raposo <braposo@gmail.com>
2018-03-07 22:01:10 +0000

    Generates shades from base colour by rotating hue instead of luminance

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const chroma = require("chroma-js");
 const hueName = require("./hue-name");
 
 const generateLums = lum => {
-    const l = lum * 10;
-    const integer = Math.floor(l);
-    const decimal = l - integer;
+    const integer = Math.floor(lum);
+    const decimal = lum - integer;
 
     // Make sure we always have the base colour as one
-    // of the shades
+    // of the shades by adding the decimal to each step
     const lums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         .map(n => n + decimal)
         .map(n => n / 10)
@@ -52,19 +51,35 @@ const createShades = (hex, lum) => {
     const lms = generateLums(lum);
 
     // Generates the different shades for each hex
-    return lms.map(lum => {
+    return lms.map(l => {
         return chroma(hex)
-            .luminance(lum)
+            .luminance(l)
             .hex();
     });
 };
 
+const createBaseShades = (hex) => {
+    const lum = chroma(hex).luminance() * 10;
+    const shades = createShades(hex, lum);
+    return shades;
+}
+
+const generateShades = (base, hue) => {
+    // Go through base shades and generate equivalent for each hue
+    const shades = base.map((shade) => {
+        const [h, s, l] = chroma(shade).hsl();
+        return chroma.hsl(hue, s, l).hex();
+    })
+
+    return shades;
+}
+
 // Mappers
 const keyword = hex => {
     const [hue, sat] = chroma(hex).hsl();
-    if (sat < 0.5) {
-        return "gray";
-    }
+    // if (sat < 0.5) {
+    //     return "gray";
+    // }
     const name = hueName(hue);
     return name;
 };
@@ -84,26 +99,31 @@ const palx = (hex, hues = 12, options = {}) => {
 
     // Set number of different colours
     //const generatedHues = createHues(hues)(hue);
-    const generatedHues = [hue, 120, 48, 30, 249]
+    const generatedHues = [hue, 278, 348, 167]
 
+    // Generate the base shades from the original colour
+    const baseShades = createBaseShades(hex);
+
+    // Get remaining shades from list of hues
     generatedHues.forEach(h => {
         // Get colour with same saturation and lightness than base for each hue
         const c = chroma.hsl(h, sat, lte);
         const key = keyword(c);
+
         colors.push({
             key,
-            value: createShades("" + c.hex(), lum),
+            value: generateShades(baseShades, h)
         });
     });
 
     colors.push({
         key: "black",
-        value: createBlack("" + color.hex()),
+        value: createBlack(hex),
     });
 
     colors.push({
         key: "gray",
-        value: createShades(desat(1 / 8)("" + color.hex()), lum),
+        value: createShades(desat(1 / 8)(hex), lum),
     });
 
     const obj = Object.assign(
