082bf410cd21d0bcbf3f45c71e3799a74cbcd9a4
Bernardo Raposo <braposo@gmail.com>
2018-05-11 18:10:29 +0100

    Displays all colours in the same row and shows additional info

--- server/components/Chip.js
+++ server/components/Chip.js
@@ -1,40 +1,149 @@
-const { createElement: h } = require('react')
-const Base = require('./Base')
+const { createElement: h } = require("react");
+const Base = require("./Base");
+const chroma = require("chroma-js");
 
-const cap = str => str.charAt(0).toUpperCase() + str.slice(1)
+const cap = str => str.charAt(0).toUpperCase() + str.slice(1);
 
 const format = str => {
-  return cap(str).replace(/\d$/, ' $&')
-}
+    return cap(str).replace(/\d$/, " $&");
+};
 
-module.exports = ({ name, color }) => h(Base, {
-    p: 2,
-    width: [
-      1/2,
-      1/3,
-      1/5
-    ]
-  },
-  h(Base, {},
-    h(Base, {
-      px: 3,
-      py: 4,
-      bg: color
-    }),
-    h(Base, {
-      css: {
-        display: 'flex'
-      }
-    },
-      h(Base, {
-        fontSize: 0,
-        fontWeight: 'bold'
-      }, format(name)),
-      h(Base, { css: { flex: '1 1 auto' } }),
-      h(Base, {
-        fontSize: 0
-      }, color)
-    )
-  )
-)
-
+module.exports = ({ name, color }) =>
+    h(
+        Base,
+        {
+            p: 1,
+            width: [1 / 2, 1 / 3, 1 / 10],
+        },
+        h(
+            Base,
+            {},
+            h(Base, {
+                px: 3,
+                py: 4,
+                bg: color,
+            }),
+            h(
+                Base,
+                {
+                    p: 1,
+                    css: {
+                        display: "flex",
+                    },
+                },
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                        fontWeight: "bold",
+                    },
+                    name
+                ),
+                h(Base, { css: { flex: "1 1 auto" } }),
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                    },
+                    color
+                )
+            ),
+            h(
+                Base,
+                {
+                    p: 1,
+                    css: {
+                        display: "flex",
+                    },
+                },
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                    },
+                    chroma(color).hsl().map(v => parseFloat(v).toFixed(2)).join(" / ")
+                )
+            ),
+            h(
+                Base,
+                {
+                    p: 1,
+                    css: {
+                        display: "flex",
+                    },
+                },
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                        fontWeight: "bold",
+                    },
+                    "Luminance"
+                ),
+                h(Base, { css: { flex: "1 1 auto" } }),
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                    },
+                    parseFloat(chroma(color).luminance()).toFixed(2)
+                )
+            ),
+            h(
+                Base,
+                {
+                    p: 1,
+                    css: {
+                        display: "flex",
+                    },
+                },
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                        fontWeight: "bold",
+                    },
+                    "C/W"
+                ),
+                h(Base, { css: { flex: "1 1 auto" } }),
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                        color: parseFloat(chroma.contrast(color, "#fff")).toFixed(2) < 3 ? "red" : "currentColor",
+                    },
+                    parseFloat(chroma.contrast(color, "#fff")).toFixed(2)
+                )
+            ),
+            h(
+                Base,
+                {
+                    p: 1,
+                    css: {
+                        display: "flex",
+                    },
+                },
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                        fontWeight: "bold",
+                    },
+                    "C/B"
+                ),
+                h(Base, { css: { flex: "1 1 auto" } }),
+                h(
+                    Base,
+                    {
+                        fontSize: 0,
+                        color: parseFloat(chroma.contrast(color, "#000")).toFixed(2) < 3 ? "red" : "currentColor",
+                    },
+                    parseFloat(chroma.contrast(color, "#000")).toFixed(2)
+                )
+            )
+            // h(Base, {
+            //   fontSize: 0,
+            //   fontWeight: 'bold'
+            // }, chroma(color).hsl().map(v => parseFloat(v).toFixed(2)).join(" / ")),
+        )
+    );
--- server/components/ChipRow.js
+++ server/components/ChipRow.js
@@ -12,32 +12,6 @@ module.exports = ({ name, values }) =>
         h(
             Base,
             {
-                is: "h2",
-                fontSize: 2,
-                mb: 1,
-            },
-            `Selected - ${name}`
-        ),
-        h(
-            Base,
-            {
-                mx: -2,
-                css: {
-                    display: "flex",
-                    flexWrap: "wrap",
-                },
-            },
-            values.slice(0, 5).map((v, i) =>
-                h(Chip, {
-                    key: i,
-                    name: name + i,
-                    color: v,
-                })
-            )
-        ),
-        h(
-            Base,
-            {
                 is: "h4",
                 fontSize: 2,
                 mb: 1,
@@ -53,10 +27,10 @@ module.exports = ({ name, values }) =>
                     flexWrap: "wrap",
                 },
             },
-            values.slice(5).map((v, i) =>
+            values.map((v, i) =>
                 h(Chip, {
                     key: i,
-                    name: name + i,
+                    name: i,
                     color: v,
                 })
             )
